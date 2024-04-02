8c920e4563097c125f3e096f47fc362ccc8dcd23
Ilya <sange0337@gmail.com>
2024-04-02 08:45:35 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(title, author, pages, editionYear, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.editionYear = editionYear;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
